42e3c422169f08d161e7a2ba8ab36c371f27508b
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-23 14:51:59 -0800

    working version needs css styling

--- app.js
+++ app.js
@@ -48,29 +48,39 @@ $(document).ready(function(event){
     this.questions = [];
   };
 
+  Bartender.prototype.greet = function(name) {
+    var name = name.charAt(0).toUpperCase() + name.slice(1); //capitalize Name
+     $(".container").prepend("<h5 class='greet'>Welcome to the Pirate Cantine, " + name + "! Let me fix ye a drink!</h5>");
+    //  $(".greet").animate({height:'toggle'}, 1500);
+  };
+
   Bartender.prototype.addCustomer = function(customer) {
     this.customers[customer.name] = customer;
   };
 
+  //if customer is known, dont ask questions, give him his preferred drink
   Bartender.prototype.checkCustomer = function(name) {
     if (this.customers[name]) {
-      $(".container").append("<h5>Welcome back " + name + "!</h5>");
-        this.customers[name].drink.name;
+      $(".container").append("<h5 class='greet'>Welcome back " + name + "!</h5>");
+      // $(".greet").animate({ height:'toggle' },1500);
+      var myDrink = this.customers[name].drink;
       $(".showDrink").remove();
       $(".container").append("<div class='showDrink'>" +
-                             "<h5>Here you go, " + name + "! This is your " + this.customers[name].drink.name + "! " + "It's made from a " + this.customers[name].drink.ingredients +  ".</h5><div>");
-      $(".showDrink").append("<h5>Would you like another drink, " + name + " ?</h5>");
-      $(".showDrink").append("<input id='moreDrinks' type='submit' value='Yes'>" +
-                             "<input id='noMoreDrinks' type='submit' value='No'>");
-
+                             "<h5>Here you go, " + name + "! This is your " + myDrink.name + "! " + "It's made from a " + formatedIngredients(customer) +  ".</h5><div>");
+      $(".showDrink").append("<h5>Would you like another drink?</h5>");
+      $(".showDrink").append("<input class='moreDrinks' type='submit' value='Yes'>" +
+                             "<input class='noMoreDrinks' type='submit' value='No'>");
+      bartender.askMoreDrinks();
     } else {
-      this.greet(name);
-      customer = new Customer(name);
-      this.addCustomer(customer); //remember customer
-      $(".choices").removeClass("hidden"); //show bartender question
-      $(".inputName").addClass("hidden"); //hide name input box
+        this.greet(name);
+        customer = new Customer(name);
+        this.addCustomer(customer); //remember customer
+        $(".choices").removeClass("hidden"); //show bartender question
+        $(".inputName").addClass("hidden"); //hide name input box
+        currentQuestion = 0;
+        bartender.askQuestion(); //begin asking questions
+      };
     };
-  };
 
   Bartender.prototype.addQuestion = function(question) {
     this.questions.push(question);
@@ -78,12 +88,12 @@ $(document).ready(function(event){
 
   var currentQuestion = 0;
   Bartender.prototype.askQuestion = function() {
-    $(".questions").empty();
+    $(".question").empty();
     if (currentQuestion > this.questions.length - 1) {
       $(".choices").addClass("hidden");
-      giveDrink();
+      bartender.giveDrink();
     } else {
-      $(".questions").append("<h5>" + this.questions[currentQuestion].question + "</h5>");
+      $(".question").append("<h5>" + this.questions[currentQuestion].question + "</h5>");
     };
   };
 
@@ -97,12 +107,8 @@ $(document).ready(function(event){
     return drink;
   }
 
-  Bartender.prototype.greet = function(name) {
-     $(".container").prepend("<h5 class='greet'>Welcome to the Tavern, " + name + "!</h5>");
-  };
-
   Bartender.prototype.nameDrink = function() {
-    var adjectives = ["Angry", "Wild", "Vicious", "Buried", "Captain's", "Bloody", "Foul", "Stinky", "Nautical", "White", "Red", "Sea", "Skinny", "Tropical", "Fuzzy", "Blackbeard's", "Gay", "Wrecked", "Broken", "Helm's"];
+    var adjectives = ["Angry", "Wild", "Vicious", "Buried", "Captain's", "Bloody", "Foul", "Stinky", "Nautical", "White", "Red", "Sea", "Skinny", "Tropical", "Fuzzy", "Blackbeard's", "Frothy", "Wrecked", "Broken", "Helm's"];
     var nouns = ["Pirate", "Parrot", "Wooden Leg", "Eye Patch", "Sail", "Barrel", "Treasure", "Table", "Booty", "Ghost", "Plank", "Float", "Skull", "Flag", "Skeleton", "Kraken", "Boat", "Bones", "Witch", "Song"];
     var random = Math.floor(Math.random() * adjectives.length);
     var adjective = adjectives[random];
@@ -110,16 +116,29 @@ $(document).ready(function(event){
     return adjective + " " + noun;
   };
 
+  Bartender.prototype.giveDrink = function() {
+    var myDrink = bartender.makeDrink(pantry, customer.preferences);
+    customer.drink = myDrink;
+    $(".container").append("<div class='showDrink'>" +
+                           "<h5>Here you go, " + customer.name + "! This is your " + customer.drink.name + "! " + "It's made from a " + formatedIngredients(customer) +  ".</h5><div>");
+    $(".showDrink").append("<h5>Would you like another drink?</h5>" +
+                           "<input class='moreDrinks' type='submit' value='Yes'>" +
+                           "<input class='noMoreDrinks' type='submit' value='No'>");
+    bartender.askMoreDrinks();
+  };
+
   Bartender.prototype.askMoreDrinks = function() {
-    $('.moreDrinks').click(function() { ///use jquery
+    $('.moreDrinks').click(function() {
       $(".showDrink").remove();
-      giveDrink();
+      bartender.giveDrink();
     });
-    $('.noMoreDrinks').click(function() { ///use jquery
+    $('.noMoreDrinks').click(function() {
       $(".showDrink").remove();
+      $(".inputName").addClass("hidden");
       $(".container").append("<h5 class='goodbye'>Thank you for stopping by, now get out...</h5>");
       $(".container").append("<input id='restart' type='submit' value='Restart'>");
       $('#restart').click(function() {
+        $(".showDrink").empty();
         $("#restart").remove();
         $(".goodbye").remove();
         $(".inputName").removeClass("hidden");
@@ -127,6 +146,10 @@ $(document).ready(function(event){
     });
   };
 
+function formatedIngredients(customer) {
+  var ingredientsList = [customer.drink.ingredients.slice(0,-1).join(", "), customer.drink.ingredients.slice(-1)[0]].join(customer.drink.ingredients.length < 2 ? "" : ", and ");
+  return ingredientsList;
+}
   //create pantry and add the following ingredients to the pantry
   //strong ingredients
   var pantry = new Pantry();
@@ -160,41 +183,13 @@ $(document).ready(function(event){
   bartender.addQuestion(new Question("Do ye like it with a salty tang?", "salty"));
   bartender.addQuestion(new Question("Are ye a lubber who likes it bitter?", "bitter"));
   bartender.addQuestion(new Question("Are ye one for a fruity finish?", "fruity"));
-  console.log(bartender);
-
-//BEGIN JQUERY
-  // function getCustomerName() {
-  //   //if bartender knows customer, get him his favorite drink, otherwise greet new customer
-  //
-  //     showNextQuestion(); // show first question
-  // };
-
-  function giveDrink() { /////////
-    var myDrink = bartender.makeDrink(pantry, customer.preferences);
-    customer.drink = myDrink;
-    var ingredientsList = customer.drink.ingredients;
-    ingredientsList = [ingredientsList.slice(0,-1).join(", "), ingredientsList.slice(-1)[0]].join(ingredientsList.length < 2 ? "" : " and ");
-    console.log(customer);
-    console.log(bartender);
-    $(".container").append("<div class='showDrink'>" +
-                           "<h5>Here you go, " + customer.name + "! This is your " + customer.drink.name + "! " + "It's made from a " + ingredientsList +  ".</h5><div>");
-    $(".showDrink").append("<h5>Would you like another drink, " + customer.name + " ?</h5>");
-    $(".showDrink").append("<input class='moreDrinks' type='submit' value='Yes'>" +
-                           "<input class='noMoreDrinks' type='submit' value='No'>");
-    bartender.askMoreDrinks(); ////////
-  };
 
   var customer;
   $(".inputName").submit(function(event) {
+    $(".inputName").addClass("hidden  ");
     event.preventDefault();
     var name = $(".name").val();
     bartender.checkCustomer(name);
-    if ($(".container").hasClass("questions")) {
-      bartender.askQuestion();
-    } else {
-      $(".container").prepend("<div class='questions'></div>");
-      bartender.askQuestion();
-    };
   });
 
   $(".choices").submit(function(event) {
